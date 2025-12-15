bba0aa50ab5fc7ed28a989099aa08755c9b26724
Ghayda-Saify <g.safw2018@gmail.com>
2025-12-15 19:36:27 +0200

    feat(client): implement admin hotel grid

--- MyHotelApp/MyHotelApp.Client/Program.cs
+++ MyHotelApp/MyHotelApp.Client/Program.cs
@@ -1,11 +1,20 @@
+using Blazored.LocalStorage;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MyHotelApp.Client;
+using MyHotelApp.Client.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:5062/") });
+builder.Services.AddBlazoredLocalStorage();
+builder.Services.AddScoped<IAuthService, AuthService>();
 
 await builder.Build().RunAsync();
---
+++ MyHotelApp/MyHotelApp.Client/Services/IAuthService.cs
@@ -0,0 +1,13 @@
+﻿using MyHotelApp.Shared.DTOs;
+
+namespace MyHotelApp.Client.Services;
+
+
+/// <summary>
+/// 
+/// </summary>
+public interface IAuthService
+{
+    Task<LoginResponse> Login(LoginRequest loginRequest);
+    Task Logout();
+}
